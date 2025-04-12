7b3809a8c5df281a6c48666535d3c71db54a1a65
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2025-04-12 14:09:36 +0530

    chore: minor updates, example use of event loop
    Summary:
    
    Test Plan:

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,90 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
 const std = @import("std");
-const bencode = @import("bencode.zig");
+const xev = @import("xev");
+
+pub const std_options: std.Options = .{
+    .log_level = .info,
+};
+
+const log = std.log.scoped(.main);
+
+// TODO: Handle interrupts
+// https://github.com/wooster0/soft/blob/786b45fff5f0a5c9106a907b5036a2041906fdb7/examples/backends/terminal/src/main.zig#L234
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var loop = try xev.Loop.init(.{});
+    defer loop.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const address = try std.net.Address.parseIp("127.0.0.1", 8080);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const u = try xev.UDP.init(address);
+    try u.bind(address);
 
-    try bw.flush(); // Don't forget to flush!
+    var c: xev.Completion = undefined;
+    defer u.close(&loop, &c, void, null, closeCallback);
+
+    var rc: xev.Completion = undefined;
+
+    // Max size of UDP packet is 65,535
+    var buffer: [65_536]u8 = undefined;
+    const rb: xev.ReadBuffer = .{ .slice = buffer[0..] };
+
+    // not sure if this will work. Seems platform dependent.
+    var state: xev.UDP.State = undefined;
+    u.read(&loop, &rc, &state, rb, void, null, readCallback);
+    // 5s timer
+    // w.run(&loop, &c, 5000, void, null, &timerCallback);
+    log.info("init everything", .{});
+    try loop.run(.until_done);
+}
+
+fn readCallback(
+    userdata: ?*void,
+    loop: *xev.Loop,
+    c: *xev.Completion,
+    s: *xev.UDP.State,
+    addr: std.net.Address,
+    se: xev.UDP,
+    b: xev.ReadBuffer,
+    r: xev.ReadError!usize,
+) xev.CallbackAction {
+    _ = userdata;
+    _ = loop;
+    _ = se;
+
+    log.info("completion: {any} {any} {any} {any}", .{ c.result, c.flags, s, addr });
+    const read_bytes = r catch |err| blk: {
+        log.warn("error when reading {!}", .{err});
+        break :blk 0;
+    };
+
+    switch (b) {
+        .slice => |v| {
+            log.info("recieved data size {d} {s}", .{ read_bytes, v[0..read_bytes] });
+        },
+        else => unreachable,
+    }
+    return .rearm;
+}
+
+fn closeCallback(
+    userdata: ?*void,
+    loop: *xev.Loop,
+    c: *xev.Completion,
+    s: xev.UDP,
+    result: xev.CloseError!void,
+) xev.CallbackAction {
+    _ = userdata;
+    _ = loop;
+    _ = c;
+    _ = s;
+    _ = result catch unreachable;
+    log.warn("shutting down server", .{});
+    return .disarm;
+}
+
+test {
+    std.testing.log_level = .debug;
+    _ = @import("bencode.zig");
+    _ = @import("metainfo.zig");
+    _ = @import("tracker/factory.zig");
 }
--- src/tracker/udp.zig
+++ src/tracker/udp.zig
@@ -1,19 +1,26 @@
 const std = @import("std");
+const xev = @import("xev");
 const interface = @import("interface.zig");
 
 const log = std.log.scoped(.tracker);
 
 pub const Tracker = struct {
+    announceCallback: interface.Tracker.AnnounceFunctionCallback,
+    event_loop: *xev.Loop,
+
     const Self = @This();
 
-    fn announce(_: *anyopaque, _: interface.Tracker.Event) !void {
-        // const self: *UDPTracker = @ptrCast(@alignCast(ptr));
+    fn announce(ptr: *anyopaque, _: interface.Tracker.AnnounceRequest) !void {
+        const self: *Tracker = @ptrCast(@alignCast(ptr));
+
+        // TODO: catch and log the error
+        try self.announceCallback(undefined);
     }
 
     pub fn adapt(self: *Self) interface.Tracker {
         return .{
             .ptr = self,
-            .announceFn = announce,
+            ._announceFn = announce,
         };
     }
 };
